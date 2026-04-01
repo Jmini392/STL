d62b94086b5df9ae977c9a7c0cfdae92a885ae08
Jmini392 <131459220+Jmini392@users.noreply.github.com>
2026-04-01 13:18:29 +0900

    5주차

--- 메인.cpp
+++ 메인.cpp
@@ -1,39 +1,37 @@
 //------------------------------------------------------------------------------------------------------------
-// 2026 1학기 STL 화56 수34		3월 25일                                                             (4주 2일)
+// 2026 1학기 STL 화56 수34		4월 1일                                                              (5주 2일)
 //------------------------------------------------------------------------------------------------------------
-// callable type
+// 
 //------------------------------------------------------------------------------------------------------------
 #include <iostream>
-#include <random>
 #include <array>
-#include <print>
 #include <ranges>
+#include <fstream>
 #include "save.h"
 using namespace std;
 
-// 랜덤값을 갖는 int 1000만개를 메모리에 저장하라.
-// 오름차순으로 정렬한 후 화면에 출력하라.
+class Dog {
+public:
+	int getId() const { return id; }
+	string getName() const { return name; }
+private:
+	string name;	// [1 , 16]
+	int id;			// [1, 999'9999]
+};
 
-default_random_engine dre;
-uniform_int_distribution uid{ 0, 999'9999 };
+// [문제] 바이너리 파일 "Dog천만마리"에는 Dog 객체 write로 천만객체를 기록하였다.
+// array에 모든 객체를 읽어와라.
+// 앞에서 100개를 화면에 출력하라.
 
-array<int, 1000'0000> a;
-
-int 오름차순 (const void * a, const void* b) {
-	return *(int*)a - *(int*)b;
-}
+array <Dog, 1000'0000> dogs;
 
 int main() {
-	for (int& num : a)
-		num = uid(dre);
+	ifstream in{ "Dog천만마리", ios::binary };
+	if (not in) {
+		cout << "파일을 열 수 없습니다." << endl;
+		return 1;
+	}
 	
-	// 여기서 qsort를 사용하여 오름차순으로 정렬하라
-	qsort(a.data(), a.size(), sizeof(array<int,1000'0000>::value_type), 오름차순);
-
-	cout << "정렬 후 - 앞에서 부터 1000개만 출력" << endl;
-	for (int num : a | views::take(1000))
-		print("{:8}", num);
-
 	save("메인.cpp");
 }
 
